53a35da603b74d4d9bdda13906ac093e2ca2a2a2
SenanuFC <sfiamcoblavie3@gatech.edu>
2023-12-24 01:46:06 +0000

    Initialize project as base

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
